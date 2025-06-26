9957bcd4b7f418bf53f3316a95952d95bbe34e75
Astreytal <sekret.vad@gmail.com>
2025-06-26 17:57:25 +0300

    add loading screen

--- src/shared/hooks/useProductsList.jsx
+++ src/shared/hooks/useProductsList.jsx
@@ -1,7 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-export function useProductsList(telegramInitData) {
+export function useProductsList() {
+  const telegramInitData = window.Telegram.WebApp.initData;
   return useQuery({
     queryKey: ['products', telegramInitData],
     queryFn: async () => {
@@ -15,3 +16,17 @@ export function useProductsList(telegramInitData) {
     staleTime: 5 * 60 * 1000,
   });
 }
+
+// async function fucktestapi() {
+//   const res = await fetch('https://avtonomerspb.ru/api/products/list', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'X-Telegram-InitData': window.Telegram.WebApp.initData,
+//     },
+//   });
+
+//   const data = await res.json();
+//   console.log('Response data:', data);
+// }
+// fucktestapi();
