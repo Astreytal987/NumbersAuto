cb6efeb9c04226bb983467b79b1e988ff01da5c8
Astreytal <sekret.vad@gmail.com>
2025-06-28 12:19:31 +0300

    delite tanstack

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,17 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import './styles/reset.css';
 import './styles/tailwind.css';
 
 import CatalogPage from '../pages/CatalogPage/CatalogPage';
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
       <CatalogPage />
-    </QueryClientProvider>
   </StrictMode>,
 );
