88ea05d550750f2527393f03634b16b3a8a5ea8b
Astreytal <sekret.vad@gmail.com>
2025-06-28 13:52:07 +0300

    fix link item

--- src/shared/ui/AutoItem/AutoItem.jsx
+++ src/shared/ui/AutoItem/AutoItem.jsx
@@ -1,6 +1,10 @@
 import number from '../../../../public/number.jpg';
 
 function AutoItem({ price, status, contact, photo }) {
+  const href = contact.startsWith('@')
+      ? `https://t.me/${contact.slice(1)}`
+      : `tel:${contact}`;
+
   return (
     <>
       <div className="overflow-hidden max-w-[225px] h-auto mb-[10px] bg-[#f2f2f2] rounded-[4px]">
@@ -27,7 +31,7 @@ function AutoItem({ price, status, contact, photo }) {
         <div className="m-[20px_10px] flex flex-col gap-2">
           <p className="text-2xl font-normal text-[#222222]">{price} ₽</p>
           {/* цена */}
-          <a className="text-[#1BA1E4] text-sm font-normal" href="https://www.google.com/">
+          <a className="text-[#1BA1E4] text-sm font-normal" href={href} target="_blank" rel="noopener noreferrer">
             {contact}
           </a>
           {/* контакт */}
